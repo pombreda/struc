9b031cda5d004b879904f8b6b33fee73a702ab62
invlpg <invlpg@localhost>
2011-03-29 00:16:05 +0800

    doctest, unbound_fields to _unbound_fields

--- struc.py
+++ struc.py
@@ -1,35 +1,53 @@
-# Created @ 2011-01-20
+"""
+Struc is a syntax wrapper for ctypes.Structure
+
+>>> class BYTE(Struc):
+...     core = ctypes.c_ubyte
+
+>>> class User(Struc):
+...     BYTE.name[32]
+...     BYTE.password[32]
+
+>>> class User2(ctypes.Structure):
+...     _fields_ = [
+...         ('name', ctypes.c_ubyte * 32),
+...         ('password', ctypes.c_ubyte * 32),
+...     ]
+
+>>> User()._fields_ == User2()._fields_
+True
+
+"""
 
 import ctypes
 
 
-unbound_fields = []
-
+_unbound_fields = []
 
 class StrucMeta(type):
 
     @staticmethod
     def __new__(cls, name, bases, attrs):
-        rv = super(StrucMeta    , cls).__new__(cls, name, bases, attrs)
+        rv = super(StrucMeta, cls).__new__(cls, name, bases, attrs)
 
-        if not unbound_fields:
+        if not _unbound_fields:
             return rv
 
         class core(ctypes.Structure):
-            _fields_ = unbound_fields[:]
-            del unbound_fields[:]
+            _fields_ = _unbound_fields[:]
+            del _unbound_fields[:]
         core.__name__ = name
         
         rv.core = core            
         return rv
         
     def __getattr__(self, name):
-        unbound_fields.append((name, self.core))
+        _unbound_fields.append((name, self.core))
         return self
         
     def __getitem__(self, index):
-        name, core = unbound_fields.pop()
-        unbound_fields.append((name, core * index))
+        name, core = _unbound_fields.pop()
+        _unbound_fields.append((name, core * index))
         return self
         
             
@@ -43,6 +61,7 @@ class Struc(object):
 
 if __name__ == '__main__':
     import doctest
+    doctest.testmod()
     
 
 
