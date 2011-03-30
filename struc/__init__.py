17ac7c8c36bbd72e5947bf4bf6a20f4e71f55be3
invlpg <invlpg@localhost>
2011-03-30 13:41:02 +0800

    Union supported

--- struc/__init__.py
+++ struc/__init__.py
@@ -14,6 +14,15 @@ Struc is a syntax wrapper for ctypes.Structure
 >>> User()._fields_ == User2()._fields_
 True
 
+
+>>> class TestUnion(Union):
+...     BYTE.foo
+...     BYTE.bar
+
+>>> u = TestUnion()
+>>> u.foo = 1
+>>> u.bar
+1
 """
 
 import ctypes
@@ -57,6 +66,42 @@ class Struc(object):
         return cls.core.__new__(cls.core, *args, **kw)
 
 
+class UnionMeta(type):
+
+    @staticmethod
+    def __new__(cls, name, bases, attrs):
+        instance = super(UnionMeta, cls).__new__(cls, name, bases, attrs)
+
+        if not _unbound_fields:
+            return instance
+
+        class core(ctypes.Union):
+            _fields_ = _unbound_fields[:]
+            del _unbound_fields[:]
+        core.__name__ = name
+        
+        instance.core = core            
+        return instance
+        
+    def __getattr__(self, name):
+        _unbound_fields.append((name, self.core))
+        return self
+        
+    def __getitem__(self, index):
+        name, core = _unbound_fields.pop()
+        _unbound_fields.append((name, core * index))
+        return self
+        
+        
+class Union(object):
+
+    __metaclass__ = UnionMeta
+
+    @staticmethod
+    def __new__(cls, *args, **kw):
+        return cls.core.__new__(cls.core, *args, **kw)
+        
+        
 class BYTE(Struc):
     core = ctypes.c_ubyte
     
